ee6de89a9c5f787727bb3efacff37c1cd03eb13e
Jayrald07 <geraldempino@gmail.com>
2023-05-03 22:12:35 +0000

    udpate

--- initialize.sh
+++ initialize.sh
@@ -13,7 +13,6 @@ PEER_PORT=0
 NODE_OU=""
 MSP=""
 SERVER_IP=""
-FORMATTED_IP="$(echo $SERVER_IP | sed 's/\./-/g')"
 
 while [[ $# -ge 1 ]] ; do
     arg="$1"
@@ -43,7 +42,6 @@ while [[ $# -ge 1 ]] ; do
         ;;
     --ca-port )
         CA_PORT="$2"
-        NODE_OU="${FORMATTED_IP}-${CA_PORT}-ca-${ORG_NAME}"
         shift
         ;;
     --ca-orderer-username )
@@ -81,6 +79,9 @@ while [[ $# -ge 1 ]] ; do
     esac
     shift
 done
+
+FORMATTED_IP="$(echo $SERVER_IP | sed 's/\./-/g')"
+NODE_OU="${FORMATTED_IP}-${CA_PORT}-ca-${ORG_NAME}"
 
 createIdentity() {
     mkdir -p $PWD/organizations/peerOrganizations/$ORG_NAME.com
